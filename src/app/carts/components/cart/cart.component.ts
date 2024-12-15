98dbd6b5609d4000cc1cce4188c267b2f18e9121
Mustafa-Zayed <mostafa_31070285@ics.tanta.edu.eg>
2024-12-15 18:17:26 +0200

    Cart page updated

--- src/app/carts/components/cart/cart.component.ts
+++ src/app/carts/components/cart/cart.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,22 +7,113 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit, OnDestroy {
-  cartItems: any = [];
-  loading: boolean = false;
+  cartItems: { product: any; quantity: number }[] = [];
+  totalPrice: number = 0;
+  cartSentSuccessfully: boolean = false;
 
-  constructor() {}
+  // Properties to hold modal data
+  @ViewChild('modalId') modalElement!: ElementRef;
+  deleteModalProd: any = null;
+  delOrClearCartFlag: string = '';
+
+  constructor(private cartService :CartService) {}
 
   ngOnInit(): void {
     // console.log('Cart Component');
-    this.loading = true;
     this.cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+    this.calcTotalPrice();
     console.log(this.cartItems);
-    // this.loading = false;
+  }
+
+  calcTotalPrice() {
+    let total = 0;
+    this.cartItems.forEach((item: any) => {
+      total += item.quantity * item.product.price;
+    });
+    this.totalPrice = total;
+  }
+  
+  updateTotalAndLocalStorage() {
+    this.calcTotalPrice();
+    localStorage.setItem('cartItems', JSON.stringify(this.cartItems));
+  }
+
+  plus(event: any) {
+    const index = this.cartItems.findIndex(
+      (item: { product: any; quantity: number }) =>
+        item.product.id === event.product.id
+    );
+    this.cartItems[index].quantity += 1;
+
+    this.updateTotalAndLocalStorage();
+    console.log(this.cartItems);
+  }
+
+  minus(event: any) {
+    const index = this.cartItems.findIndex(
+      (item: { product: any; quantity: number }) =>
+        item.product.id === event.product.id
+    );
+    this.cartItems[index].quantity -= 1;
+
+    this.updateTotalAndLocalStorage();
+    console.log(this.cartItems);
+  }
+
+  addAmount(index: number) {
+    this.cartItems[index].quantity += 1;
+
+    this.updateTotalAndLocalStorage();
+    console.log(this.cartItems);
+  }
+  
+  minsAmount(index: number) {
+    this.cartItems[index].quantity -= 1;
+
+    this.updateTotalAndLocalStorage()
+    console.log(this.cartItems);
+  }
+
+  showDeleteOrClearCartModal(delOrClearCartFlag: string, product?: any) {
+    this.delOrClearCartFlag = delOrClearCartFlag;
+
+    this.deleteModalProd = product;
+  
+    const modal = new (window as any).bootstrap.Modal(this.modalElement.nativeElement);
+    modal.show();
+  }
+
+  deleteProductFromCart() {
+    this.cartItems = this.cartItems.filter(
+      (item: { product: any; quantity: number }) =>
+        item.product.id !== this.deleteModalProd.id
+    )
+
+    this.updateTotalAndLocalStorage();
+    console.log(this.cartItems);
+  }
+
+  clearCart(){
+    this.cartItems = [];
+
+    this.updateTotalAndLocalStorage();
+    console.log(this.cartItems);
+  }
+
+  sendCart(){
+    this.cartService.sendCart(this.cartItems).subscribe(next => {
+      console.log(next);
+      // this.clearCart();
+      setTimeout(() => {
+        this.cartSentSuccessfully = false;
+      }, 3000);
+      this.cartSentSuccessfully = true;
+    });
   }
 
   ngOnDestroy(): void {
-    localStorage.removeItem('cartItems');
-    this.cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
-    console.log(this.cartItems);
+    // localStorage.removeItem('cartItems');
+    // this.cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+    // console.log(this.cartItems);
   }
 }
--- src/app/carts/services/cart.service.ts
+++ src/app/carts/services/cart.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  sendCart(cartItems: { product: any; quantity: number }[]) {
+    let prodsIdAndQuantity: { productId: number; quantity: number }[] =[];
+
+    // cartItems.forEach((item) =>
+    //   prodsIdAndQuantity.push({
+    //     productId: item.product.id,
+    //     quantity: item.quantity,
+    //   })
+    // );
+    prodsIdAndQuantity = cartItems.map((item) => ({
+      productId: item.product.id,
+      quantity: item.quantity,
+    }))
+    // console.log(prodsIdAndQuantity);
+    // console.log(cartItems);
+
+    let body = {
+      userId:5, // suppose to be the user token got from login, keep it static for now
+      date:new Date(),
+      products:prodsIdAndQuantity
+    }
+    // console.log(body);
+
+    return this.http.post(`${environment.API_URL}/carts`, (body));
+  }
 }
